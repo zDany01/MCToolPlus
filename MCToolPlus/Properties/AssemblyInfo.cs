2b68371f81975eece735be943367d69a7c020c85
zDany01 <85403430+zDany01@users.noreply.github.com>
2021-08-01 19:43:47 +0200

    Fixed fileversion

--- MCToolPlus/Properties/AssemblyInfo.cs
+++ MCToolPlus/Properties/AssemblyInfo.cs
@@ -35,5 +35,4 @@ using System.Runtime.InteropServices;
 // È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
 // usando l'asterisco '*' come illustrato di seguito:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.54.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.54.0")]
